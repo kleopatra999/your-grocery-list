6f46deca5df0ac32ac2930e85158296434c3c59a
unknown <Tarini@.(none)>
2014-06-02 22:49:58 +0530

    spring config and properties added

---
+++ src/main/java/com/groc/framework/util/PropertyUtil.java
@@ -0,0 +1,13 @@
+package com.groc.framework.util;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+public class PropertyUtil extends PropertyPlaceholderConfigurer{
+	
+	private static Properties appProperties = null;
+	
+	
+
+}
